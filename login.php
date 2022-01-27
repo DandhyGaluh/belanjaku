66138e1876f042d7310d038d58def2ab0ea3d593
AchmadBudy <56147721+AchmadBudy@users.noreply.github.com>
2022-01-27 12:08:42 +0700

    update line 12
    tadi typo ada "

--- login.php
+++ login.php
@@ -9,10 +9,10 @@
 <body id="bg-login">
     <div class="box-login">
         <h2>Login</h2>
-        <form" action="" method="POST">
-        <input type="text" name="user" placeholder="Username" class="input-control">
-        <input type="password" name="pass" placeholder="Password" class="input-control">
-        <input type="submit" name="submit" value="Login" class="btn">
+        <form action="" method="POST">
+            <input type="text" name="user" placeholder="Username" class="input-control">
+            <input type="password" name="pass" placeholder="Password" class="input-control">
+            <input type="submit" name="submit" value="Login" class="btn">
         </form>
     <?php 
         if(isset($_POST['submit'])){
